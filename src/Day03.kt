711d78958450d4b0ee6e38b15944a12b5a9849e2
Ivan Zykov <ivan.v.zykov@gmail.com>
2025-06-11 17:06:32 +0200

    Day 3. Solve part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -7,15 +7,15 @@ fun main() {
             .sumOf { numberCommaNumber -> numberCommaNumber.substringBefore(",").toInt() * numberCommaNumber.substringAfter(",").toInt() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int = input.joinToString().splitToSequence("do()")
+        .map { piece -> piece.substringBefore("don't()") }
+        .sumOf { cleanPiece -> part1(listOf(cleanPiece)) }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 161)
 
-    val inputPart1 = readInput("Day03_part1")
-    check(part1(inputPart1) == 159833790)
-//    part2(input).println()
+    val input = readInput("Day03")
+    check(part1(input) == 159833790)
+    check(part2(input) == 89349241)
 }
