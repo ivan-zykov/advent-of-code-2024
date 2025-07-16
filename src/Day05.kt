101dfb6c38e8ec0932a43446824c64eac62f4839
Ivan Zykov <ivan.v.zykov@gmail.com>
2025-07-16 12:44:36 +0300

    Day 5. Extract part 1 to smaller functions

--- src/Day05.kt
+++ src/Day05.kt
@@ -8,24 +8,22 @@ fun main() {
             // None of the prev pages are in the after rules
             // None of the after pages are in the before rules
 
-        val rules = input.subList(0, input.indexOf(""))
-            .map { it.substringBefore('|') to it.substringAfter('|') }
-        val updates = input.subList(input.indexOf("") + 1, input.lastIndex + 1)
-            .map { it.split(',') }
+        val rules = getRulesFrom(input)
+        val updates = getUpdatesFrom(input)
 
-        val rulesAfter = rules.groupBy(keySelector = { it.first }, valueTransform = { it.second })
-        val rulesBefore = rules.groupBy(keySelector = { it.second }, valueTransform = { it.first })
+        val rulesAfter = rules.rulesAfter()
+        val rulesBefore = rules.rulesBefore()
 
         return updates.filter { update ->
             update.all { page ->
-                val pagesBefore = update.subList(0, update.indexOf(page))
-                val pagesAfter = update.subList(update.indexOf(page), update.lastIndex + 1)
+                val pagesBefore = update.pagesBefore(page)
+                val pagesAfter = update.pagesAfter(page)
 
-                pagesBefore.all { rulesAfter[page]?.contains(it) != true } &&
-                        pagesAfter.all { rulesBefore[page]?.contains(it) != true }
+                pagesBefore.follow(rulesAfter[page]) &&
+                        pagesAfter.follow(rulesBefore[page])
             }
         }.sumOf { update ->
-            update[update.lastIndex / 2].toInt()
+            update.middlePage()
         }
     }
 
@@ -41,3 +39,24 @@ fun main() {
     check(part1(input) == 3608)
 //    part2(input).println()
 }
+
+private fun List<String>.middlePage(): Int = this[lastIndex / 2].toInt()
+
+private fun List<String>.pagesBefore(page: String) = subList(0, indexOf(page))
+
+private fun List<String>.pagesAfter(page: String): List<String> = subList(indexOf(page), lastIndex + 1)
+
+private fun List<String>.follow(rulesAfterForPage: List<String>?) =
+    all { rulesAfterForPage?.contains(it) != true }
+
+private fun List<Pair<String, String>>.rulesBefore() =
+    groupBy(keySelector = { it.second }, valueTransform = { it.first })
+
+private fun List<Pair<String, String>>.rulesAfter() =
+    groupBy(keySelector = { it.first }, valueTransform = { it.second })
+
+private fun getUpdatesFrom(input: List<String>) = input.subList(input.indexOf("") + 1, input.lastIndex + 1)
+    .map { it.split(',') }
+
+private fun getRulesFrom(input: List<String>) = input.subList(0, input.indexOf(""))
+    .map { it.substringBefore('|') to it.substringAfter('|') }
