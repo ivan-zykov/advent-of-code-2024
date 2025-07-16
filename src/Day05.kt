22f829243f6511c1a0ab39efa6442ffacde1f34e
Ivan Zykov <ivan.v.zykov@gmail.com>
2025-07-16 12:47:14 +0300

    Day 5. WIP Solve part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,3 +1,5 @@
+import kotlin.collections.forEach
+
 fun main() {
     fun part1(input: List<String>): Int {
 
@@ -28,26 +30,77 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        /*
+        Get rules and updates: reuse part1
+        Get incorrect updates: reuse part1
+        Sort updates with a var of bubble sort:
+            Page iterator
+            For each page, check after rules
+                If rules not satisfied, swap with next
+         */
+
+        val rules = getRulesFrom(input)
+        val updates = getUpdatesFrom(input)
+        val rulesGrouped = rules.rulesAfter()
+
+        val wrongUpdates = updates.filterNot { update ->
+            update.all { page ->
+                val pagesBefore = update.pagesBefore(page)
+                pagesBefore.follow(rulesGrouped[page])
+            }
+        }
+
+        val fixedUpdates: List<List<String>> = buildList {
+            wrongUpdates.forEach { update ->
+                val temp = update.toMutableList()
+
+                for (i in temp.indices) {
+                    var wereSwapped = false
+                    for (j in 0..temp.lastIndex - i - 1) {
+                        val pagesAfter = temp.pagesAfter(j)
+                        val pagesAfterAllNextPages: Set<String> = buildSet {
+                            pagesAfter.forEach { pageAfter ->
+                                rulesGrouped[pageAfter]?.let { addAll(it) }
+                            }
+                        }
+                        if (temp[j] in pagesAfterAllNextPages) {
+                            temp.swapWithNextAt(j)
+                            wereSwapped = true
+                        }
+                    }
+                    if (!wereSwapped) {
+                        break
+                    }
+                }
+
+                add(temp.toList())
+            }
+        }
+
+        return fixedUpdates.sumOf {
+            it.middlePage()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 143)
+    check(part2(testInput) == 123)
 
     val input = readInput("Day05")
     check(part1(input) == 3608)
-//    part2(input).println()
+    part2(input).println() // 5570 and 5055 both too high
 }
 
 private fun List<String>.middlePage(): Int = this[lastIndex / 2].toInt()
 
 private fun List<String>.pagesBefore(page: String) = subList(0, indexOf(page))
 
-private fun List<String>.pagesAfter(page: String): List<String> = subList(indexOf(page), lastIndex + 1)
+private fun List<String>.pagesAfter(idx: Int): List<String> = subList(idx + 1, lastIndex + 1)
 
-private fun List<String>.follow(rulesAfterForPage: List<String>?) =
-    all { rulesAfterForPage?.contains(it) != true }
+private fun List<String>.follow(rules: List<String>?) =
+    all { rules?.contains(it) != true }
 
 private fun List<Pair<String, String>>.rulesBefore() =
     groupBy(keySelector = { it.second }, valueTransform = { it.first })
@@ -60,3 +113,15 @@ private fun getUpdatesFrom(input: List<String>) = input.subList(input.indexOf(""
 
 private fun getRulesFrom(input: List<String>) = input.subList(0, input.indexOf(""))
     .map { it.substringBefore('|') to it.substringAfter('|') }
+
+private fun MutableList<String>.swapWithPrev(idx: Int) {
+    val temp = this[idx - 1]
+    this[idx - 1] = this[idx]
+    this[idx] = temp
+}
+
+private fun MutableList<String>.swapWithNextAt(idx: Int) {
+    val temp = this[idx + 1]
+    this[idx + 1] = this[idx]
+    this[idx] = temp
+}
